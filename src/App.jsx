5b9d0ac796ef5706a618301323c3d6f7b2c67ad8
camigalvan0 <camygalvan4@gmail.com>
2026-05-19 00:05:09 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Autos0km from './pages/Autos0km'
 import Autos from './pages/Autos'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Autosusados from './pages/AutosUsados.jsx'
 import SobreNosotros from './pages/SobreNosotros'
 import Clientes from './pages/Clientes'
 import Contacto from './pages/Contacto'
@@ -22,7 +21,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Autos0KM" element={<Autos0km />} />
         <Route path="/Autos" element={<Autos />} />
-        <Route path="/AutosUsados" element={<Autosusados />} />
         <Route path="/autos/:id" element={<AutoDetalle />} />
         <Route path="/Clientes" element={<Clientes />} />
         <Route path="/SobreNosotros" element={<SobreNosotros/>} />
